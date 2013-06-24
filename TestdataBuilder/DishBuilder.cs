9144c23ea9231d400151b8757cb1344cbf2bbac0
Nico Schroons <nico.schroons@outlook.com>
2013-06-24 20:58:26 +0200

    added testdatabuilder pattern

---
+++ TestdataBuilder/DishBuilder.cs
@@ -0,0 +1,48 @@
+﻿using DinnerManager.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestdataBuilder
+{
+    internal class DishBuilder
+    {
+        private Dish _dish;
+
+        public DishBuilder()
+        {
+            this._dish = new Dish();
+        }
+
+        public DishBuilder WithName(string name)
+        {
+            this._dish.Name = name;
+            return this;
+        }
+
+        public DishBuilder WithRating(int rating)
+        {
+            this._dish.Rating = rating;
+            return this;
+        }
+
+        public DishBuilder WithDescription(string description)
+        {
+            this._dish.Description = description;
+            return this;
+        }
+
+        public DishBuilder WithRemarks(string remarks)
+        {
+            this._dish.Remarks = remarks;
+            return this;
+        }
+
+        public Dish Build()
+        {
+            return _dish;
+        }
+    }
+}
---
+++ TestdataBuilder/MealBuilder.cs
@@ -0,0 +1,36 @@
+﻿using DinnerManager.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestdataBuilder
+{
+    internal class MealBuilder
+    {
+        private Meal _meal;
+
+        public MealBuilder()
+        {
+            this._meal = new Meal();
+        }
+
+        public MealBuilder WithDate(DateTime date)
+        {
+            this._meal.Date = date;
+            return this;
+        }
+
+        public MealBuilder WithDish(Dish dish)
+        {
+            this._meal.Dish = dish;
+            return this;
+        }
+
+        public Meal Build()
+        {
+            return _meal;
+        }
+    }
+}
