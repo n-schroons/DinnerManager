52ab36c86718928a364416fd60086a2ec5715ece
Nico Schroons <nico.schroons@outlook.com>
2013-06-26 17:19:01 +0200

    added data annotations

--- DinnerManager.Entities/Meal.cs
+++ DinnerManager.Entities/Meal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace DinnerManager.Entities
     public class Meal
     {
         public int Id { get; set; }
+        [Required]
         public DateTime Date { get; set; }
 
         public virtual Dish Dish { get; set; }
